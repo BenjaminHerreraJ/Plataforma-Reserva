07b058d6dd1c3943c06f9e50acc208fc220855f1
Benjamin <ben.herreraj@duocuc.cl>
2026-05-20 14:56:40 -0400

    Se creo la migracion y configuro la conexion a la bd

---
+++ ms-guest/src/main/resources/db/migration/V2__insert_huespedes_seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO bd_huespedes.huespedes (rut_pasaporte, nombre, email, telefono) 
+VALUES 
+('11111111-1', 'Juan Pérez', 'juan.perez@email.com', '+56912345678'),
+('22222222-2', 'María Loaiza', 'maria.l@email.com', '+56987654321'),
+('PAS123456', 'John Doe', 'john.doe@email.com', NULL);
---
+++ ms-room/src/main/resources/bd/migration/V2__insert_habitaciones_seed_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO bd_habitaciones.habitaciones (numero, tipo, precio_noche, estado) 
+VALUES 
+('101', 'Simple', 45000.0, 'Disponible'),
+('102', 'Simple', 45000.0, 'Disponible'),
+('201', 'Doble', 75000.0, 'Disponible'),
+('301', 'Suite', 120000.0, 'Ocupada');
